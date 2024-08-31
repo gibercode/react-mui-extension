cf72c368a754ef6d6e3c07edbccfe116dd9b07d0
MauriceMiot <maurice.yggdrasil@gmail.com>
2024-08-31 08:37:05 -0400

    fix build sass

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,4 +1,5 @@
 const esbuild = require('esbuild');
+const { sassPlugin } = require('esbuild-sass-plugin');
 esbuild
   .build({
     entryPoints: ['src/popup.tsx', 'src/options.tsx'],
@@ -9,5 +10,9 @@ esbuild
     target: ['chrome58', 'firefox57'],
     loader: { '.js': 'jsx', '.tsx': 'tsx' },
     define: { 'process.env.NODE_ENV': '"production"' },
+    plugins: [
+      sassPlugin(),
+    
+    ],
   })
   .catch(() => process.exit(1));
